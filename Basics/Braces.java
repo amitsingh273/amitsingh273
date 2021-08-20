319c89313b09bdd9b7fdbd05a88d3ca95dc61a90
amitsingh273 <amitsingh273@mail.com>
2021-08-20 09:26:16 +0530

    brackets ordering prog

--- Basics/Braces.java
+++ Basics/Braces.java
@@ -1,56 +1,37 @@
 package Basics;
 
 public class Braces {
-    public static int smallOpen, smallClosed, curlyOpen, curlyClosed, bigOpen, bigClosed =0;
+    public static int smallOpen, curlyOpen, bigOpen =0;
     public static void main(String[] args){
-        String braces = "[[{{(())}}]";
-        boolean numMatch = checkNumberofbrackets(braces);
-        if(numMatch){
-            checkOrders(braces);
-        }
-    }
-
-    private static boolean checkNumberofbrackets(String braces) {
-        char[] ch = braces.toCharArray();
-        smallOpen = count(ch,'(');
-        smallClosed = count(ch,')');
-        curlyOpen = count(ch,'{');
-        curlyClosed = count(ch,'}');
-        bigOpen = count(ch,'[');
-        bigClosed = count(ch,']');
-        if(smallClosed!=smallOpen) {
-            System.out.println("Mismatch in braces: ()" + smallOpen + " " + smallClosed);
-            return false;
-        }
-        else if(curlyOpen!=curlyClosed) {
-            System.out.println("Mismatch in braces: {}" + curlyOpen + " " + curlyClosed);
-            return false;
-        }
-        else if(bigOpen!=bigClosed) {
-            System.out.println("Mismatch in braces: []" + bigOpen + " " + bigClosed);
-            return false;
-        }
-        else
-            return true;
-    }
-
-    private static int count(char[] ch, char s) {
-        int count =0;
-        for(int i =0;i<=ch.length-1;i++){
-            if(s==ch[i])
-                count++;
-        }
-        return count;
+        String braces = "[[{{((()))}})]]";
+        checkOrders(braces);
     }
 
     private static void checkOrders(String braces){
-        for(int j=0;j<3;j++) {
-            int count =0;
-            for (int i = 0; i <= braces.length() - 1; i++) {
-                if(braces.charAt(i) == '('){
-                    
-                }
+        for (int i = 0; i <= braces.length() - 1; i++) {
+            if(braces.charAt(i) == '(')
+                smallOpen++;
+            else if(braces.charAt(i) == '{' && smallOpen == 0)
+                curlyOpen++;
+            else if(braces.charAt(i) == '[' && smallOpen ==0 && curlyOpen == 0)
+                bigOpen++;
+            else if(braces.charAt(i) == ')')
+                smallOpen--;
+            else if(braces.charAt(i) == '}' && smallOpen == 0)
+                curlyOpen--;
+            else if(braces.charAt(i) == '}' && smallOpen != 0) {
+                break;
+            }
+            else if(braces.charAt(i) == ']' && smallOpen == 0 && curlyOpen ==0)
+                bigOpen--;
+            else if(braces.charAt(i) == ']' && (smallOpen != 0 || curlyOpen !=0)) {
+                break;
             }
         }
+        if(smallOpen !=0 || curlyOpen !=0 || bigOpen !=0){
+            System.out.println("Brackets are not in order");
+        }
+        else
+            System.out.println("Brackets are in order");
     }
 }
